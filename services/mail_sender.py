9b014a9d526f902a827b57c793d7668696c9c9c3
VictoriaPresnyakova <victoriapresnyakova202@gmail.com>
2024-12-09 20:17:12 +0400

    feature: mail sender in new Thread

--- services/mail_sender.py
+++ services/mail_sender.py
@@ -1,4 +1,7 @@
 import smtplib
+import time
+from threading import Thread
+
 from misc.config import *
 from email.mime.text import MIMEText
 
@@ -7,8 +10,10 @@ class MailSender:
     def __init__(self):
         self.from_email = EMAIL
         self.from_password = EMAIL_PASSWORD
+        self.messages = []
+        Thread(target=self._work, daemon=True).start()
 
-    def send_email(self, to_email, subject, body):
+    def _send_email(self, to_email, subject, body):
         print(to_email, subject, body)
         msg = MIMEText(body)
         msg["Subject"] = subject
@@ -18,3 +23,20 @@ class MailSender:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
             server.login(self.from_email, self.from_password)
             server.sendmail(self.from_email, to_email, msg.as_string())
+
+    def _send_email_async(self, message):
+        self.messages.append(message)
+
+    def send_email(self, to_email, subject, body):
+        thread = Thread(target=self._send_email_async, args=({'to_email': to_email, 'subject': subject, 'body': body},), daemon=True)
+        thread.start()
+        return thread
+
+    def _work(self):
+        while True:
+            if self.messages:
+                for x in self.messages:
+                    message = self.messages.pop(0)
+                    self._send_email(message.get('to_email', ''), message.get('subject', ''), message.get('body', ''))
+            else:
+                time.sleep(1)
